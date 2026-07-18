13562dffb198dd32104c610a3318377023eb6e24
schuwarz <schuwarz@outlook.com>
2026-07-18 13:12:58 +0500

    Last step to add tanstack. General cleaning

--- src/entities/comment/model/commentApi.js
+++ src/entities/comment/model/commentApi.js
@@ -1,16 +1,7 @@
-import { STORAGE_KEYS, getItem, setItem } from '@/shared/lib/storage'
 import { instance } from '@/shared/api/axiosInstance';
 
 export async function fetchCommentsByPostId(postId) {
-  const cacheKey = STORAGE_KEYS.COMMENTS_BY_POST(postId);
-  const cached = getItem(cacheKey);
-  
-  if (Array.isArray(cached) && cached.length > 0) {
-    return cached;
-  }
-
   const res = await instance.get(`/posts/${postId}/comments`);
   const data = res.data;
-  setItem(cacheKey, data);
   return data;
 }
--- src/entities/post/model/postApi.js
+++ src/entities/post/model/postApi.js
@@ -1,25 +1,13 @@
-import { STORAGE_KEYS, getItem, setItem } from "@/shared/lib/storage";
 import { instance } from "@/shared/api/axiosInstance";
 
 export async function fetchPosts() {
-  const cached = getItem(STORAGE_KEYS.POSTS);
-  if (cached) return cached;
-
   const res = await instance.get('/posts');
   const data = res.data;
-
-  setItem(STORAGE_KEYS.POSTS, data);
   return data;
 }
 
 export async function fetchPostById(id) {
-  const cacheKey = STORAGE_KEYS.POST_BY_ID(id);
-  const cached = getItem(cacheKey);
-  if (cached) return cached;
-
   const res = await instance.get(`/posts/${id}`);
   const data = await res.data;
-  
-  setItem(cacheKey, data);
   return data;
 }
